88cc602a8cde9eb7a7ba4e6ff2f376bca38b870a
Chandra Station <81539144+ChandraStation@users.noreply.github.com>
2021-07-24 00:14:03 -0700

    Update run.sh

--- Multi Pool/run.sh
+++ Multi Pool/run.sh
@@ -5,4 +5,4 @@ if [ -z "$PAYMENT_ADDR" ]; then
   exit 1
 fi
 
-exec ./target/release/packetcrypt ann -p "$PAYMENT_ADDR" "$POOL1" "$POOL2" "$POOL3" "$POOL4"
+exec ./target/release/packetcrypt ann -p "$PAYMENT_ADDR" http://pool.pkt.world http://pool.pkteer.com http://pool.srizbi.com
